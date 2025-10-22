cbe0ecedaec5a446df4b70aabba8696d2fc3aad2
daniel.torres <daniel.torres@zabud.com.co>
2025-10-22 12:50:14 -0500

    feat: add auditory data on postgresql schema

--- src/main/resources/static/scripts_db/2025-10-21/create_table_student.sql
+++ src/main/resources/static/scripts_db/2025-10-21/create_table_student.sql
@@ -4,6 +4,11 @@ CREATE TABLE "school".student (
     name VARCHAR(256) NOT NULL,
     phone_number VARCHAR(20),
     email VARCHAR(256),
-    number BIGINT,
-    average_mark DOUBLE PRECISION
+    number INTEGER,
+    average_mark DOUBLE PRECISION,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    deactivated_at TIMESTAMP NULL,
+    updated_by VARCHAR(20) DEFAULT 'system',
+    CONSTRAINT uq_student_dni UNIQUE (dni)
 );
--- src/main/resources/static/scripts_db/2025-10-21/create_table_teacher.sql
+++ src/main/resources/static/scripts_db/2025-10-21/create_table_teacher.sql
@@ -4,5 +4,10 @@ CREATE TABLE "school".teacher (
     name VARCHAR(256) NOT NULL,
     phone_number VARCHAR(20),
     email VARCHAR(256),
-    salary DOUBLE PRECISION NOT NULL
+    salary DOUBLE PRECISION NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    deactivated_at TIMESTAMP NULL,
+    updated_by VARCHAR(20) DEFAULT 'system',
+    CONSTRAINT uq_teacher_dni UNIQUE (dni)
 );
