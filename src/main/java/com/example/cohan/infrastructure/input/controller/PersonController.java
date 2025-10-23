162a9f1e868dad9fd9489a91817f3fde08589bc1
daniel.torres <daniel.torres@zabud.com.co>
2025-10-23 10:16:50 -0500

    feat: Implement Cors Configuration

--- src/main/java/com/example/cohan/infrastructure/input/controller/PersonController.java
+++ src/main/java/com/example/cohan/infrastructure/input/controller/PersonController.java
@@ -13,10 +13,18 @@ import com.example.cohan.domain.usecase.PersonCommandUseCase;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/persons")
+@CrossOrigin(origins = "*", allowedHeaders = "*", methods = {})
 public class PersonController {
     private final PersonCommandUseCase personCommandUseCase;
 
